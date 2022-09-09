b1881047c1e15c4b9398c0a24a2dea40dc47935f
adhillon0817 <avleendhillon@gmail.com>
2022-09-08 21:55:14 -0700

    defined columns in models files

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -8,37 +8,50 @@ class Product extends Model {}
 
 // set up fields and rules for Product model
 Product.init(
-  {
+  { id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  }, 
+
     // define columns
+   
+    
+  
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false, 
+    },
+
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate:{
+        isDecimal: true,
+      }
+    },
+
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate:{
+        isNumeric: true,
+      }
+      
+    },
     category_id: {
       type: DataTypes.INTEGER,
       references: {
         model: 'category',
         key: 'id'
-      },
-    
-    category_name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      },
-    Product:{
-      type: DataTypes.INTEGER,
-      references: {
-        model: "Product",
-        key: 'id',
-        allowNull: false,
-        primaryKey: true,
-        autoIncrement: true, 
-      },
-
-
-    }
-
-    }
-    
+      },    
       //refer to read me to make category_id for each listed thing.
     }
   },
+
+
   {
     sequelize,
     timestamps: false,
